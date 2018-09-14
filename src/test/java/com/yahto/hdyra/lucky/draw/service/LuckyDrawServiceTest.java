4ee270d7569253364f0e09fec08308dbd0c5bf43
yahto <401306275@qq.com>
2018-09-14 23:29:45 +0800

    test 20000 case oom: unable to create new native thread to be fixed

--- src/test/java/com/yahto/hdyra/lucky/draw/service/LuckyDrawServiceTest.java
+++ src/test/java/com/yahto/hdyra/lucky/draw/service/LuckyDrawServiceTest.java
@@ -21,27 +21,24 @@ import java.util.concurrent.*;
 public class LuckyDrawServiceTest {
 
     private static ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
-    private static ExecutorService executorService = new ThreadPoolExecutor(200, 200,
+    private static ExecutorService executorService = new ThreadPoolExecutor(20000, 20000,
             0L, TimeUnit.MILLISECONDS,
-            new LinkedBlockingDeque<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+            new LinkedBlockingDeque<>(20480), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
     @Autowired
     private LuckyDrawService luckyDrawService;
 
-    private CountDownLatch countDownLatch = new CountDownLatch(200);
+    private CountDownLatch countDownLatch = new CountDownLatch(20000);
 
     @Test
     public void luckyDraw() throws InterruptedException {
-        for (int i = 0; i < 200; i++) {
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    countDownLatch.countDown();
-                    DrawResult drawResult = luckyDrawService.luckyDraw(1L, 12311L);
-                    System.out.println(drawResult);
-                }
+        for (int i = 0; i < 20000; i++) {
+            executorService.execute(() -> {
+                countDownLatch.countDown();
+                DrawResult drawResult = luckyDrawService.luckyDraw(1L, 12311L);
+                System.out.println(drawResult);
             });
         }
         countDownLatch.await();
-        Thread.sleep(5000);
+        Thread.sleep(50000);
     }
 }
